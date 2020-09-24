76bf80fa55a48856c2d58ea5b251ea49fc3975f9
Raulks <rksole90@gmail.com>
2020-09-24 13:58:50 +0200

    Updates

---
+++ precache-manifest.a30ae8f9582ab2d804cd539a21b56563.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a19a10707be8416a0162168219473294",
+    "url": "/break/index.html"
+  },
+  {
+    "revision": "a69ff6061b915eaa4368",
+    "url": "/break/static/css/main.ed57d21d.chunk.css"
+  },
+  {
+    "revision": "ca5c093d5a04414b3c21",
+    "url": "/break/static/js/2.ab02a3a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/break/static/js/2.ab02a3a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a69ff6061b915eaa4368",
+    "url": "/break/static/js/main.6142f82b.chunk.js"
+  },
+  {
+    "revision": "46dd28076b6288a4fd3a",
+    "url": "/break/static/js/runtime-main.da6d46ba.js"
+  },
+  {
+    "revision": "807f040f3b3cc7332a7783040f1e5d5c",
+    "url": "/break/static/media/logo.807f040f.png"
+  }
+]);
